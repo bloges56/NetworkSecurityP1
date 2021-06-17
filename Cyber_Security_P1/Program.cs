6e7cf3cb16f9a0602da400c8adbae930eba34b5d
Brady <brady.drew.logan@gmail.com>
2021-06-17 09:42:33 -0500

    first commit

---
+++ Cyber_Security_P1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Cyber_Security_P1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
